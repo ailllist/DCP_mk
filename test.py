bffefa7518edf5c1d26bc4c8ed53a45001abb486
lee hojun <hojun060534@gmail.com>
2023-06-27 21:25:28 +0900

    0628

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ import numpy as np
 
 arr1 = np.array([[[1,2,3],[4,5,6],[7,8,9]], [[1,2,3],[4,5,6],[7,8,9]]], np.float32)
 tensor1 = torch.tensor(arr1)
-print(torch.softmax(tensor1, dim=2))
+print(torch.mean(tensor1, dim=[0]))
